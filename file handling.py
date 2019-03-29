008b017172ae398e49bd7ffb9e437d1df33df8db
0827171149 <40623817+0827171149@users.noreply.github.com>
2019-03-29 12:55:06 +0530

    Add files via upload

---
+++ file handling.py
@@ -0,0 +1,9 @@
+f = open("C:/Users/STUDENT/Desktop/prasoon/myfile.txt", "x")
+f = open("C:/Users/STUDENT/Desktop/prasoon/myfile.txt", "a")
+f.write("welcome")
+f.close()
+
+
+f =open("C:/Users/STUDENT/Desktop/prasoon/handling.txt", "r")
+print(f.read())
+
